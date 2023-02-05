283af05b23552fe25ba60dbb0087efef2ecba780
shapoval-s <serhiishapoval0@gmail.com>
2023-02-05 19:14:38 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,57 @@
+// четверте завдання
+
+
+let ferst4=Number(prompt("Enter ferst month salary", 'salary$'));
+
+
+let second4=Number(prompt("Enter ferst month salary", 'salary$'));
+
+alert(`Your have bonus! Your total salary 2 was : ${ferst4 + second4} $ It's increase for 1$ and nowit is: ${ferst4 + second4 +1} $`);
+
+let summa4=ferst4 + second4+1;
+
+if (summa4 >= 2000) {
+    alert( 'I am ready to work' );
+  } else {
+    alert( 'I am Out' ); 
+  }
+
+//alert(`Will you work next?: ${summa4>=2000}`);
+
+
+
+// трете завдання
+
+
+let ferst3=Number(prompt("Enter ferst month salary", 'salary$'));
+
+
+let second3=Number(prompt("Enter ferst month salary", 'salary$'));
+
+alert(`Your have bonus! Your total salary 2 was : ${ferst3 + second3} $ It's increase for 1$ and nowit is: ${ferst3 + second3 +1} $`);
+
+let summa=ferst3 + second3+1
+
+
+alert(`Will you work next?: ${summa>=2000}`);
+
+
+// перше завдання
+let ferst=Number(prompt("Enter ferst month salary", 'salary$'));
+
+
+let second=Number(prompt("Enter ferst month salary", 'salary$'));
+
+alert(`Your salary 2 month : ${ferst + second} $`);
+
+
+// друге завдання
+let ferst2=Number(prompt("Enter ferst month salary", 'salary$'));
+
+
+let second2=Number(prompt("Enter ferst month salary", 'salary$'));
+
+alert(`Your have bonus! Your total salary 2 was : ${ferst2 + second2} $ It's increase for 1$ and nowit is: ${ferst2 + second2 +1} $`);
+
+
+
